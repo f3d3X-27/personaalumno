23a610a712d868ffaf678007d73614d5d1abeb52
Agustin Moya <agustinmoyas@gmail.com>
2020-10-10 20:52:23 -0300

    Added all repositories #2

---
+++ src/main/java/com/example/personaalumno/repositories/AutorRepository.java
@@ -0,0 +1,8 @@
+package com.example.personaalumno.repositories;
+
+import com.example.personaalumno.entities.Autor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AutorRepository extends BaseRepository<Autor, Long> {
+}
